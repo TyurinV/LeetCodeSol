9e6e849d4fc7612204d61b9b843e21f10c50e532
TyurinV <denfal486@gmail.com>
2023-04-17 23:22:21 +0300

    some fix

--- src/main/java/ru/testikov/leet/L1137Tribonacci_Number.java
+++ src/main/java/ru/testikov/leet/L1137Tribonacci_Number.java
@@ -1,5 +1,7 @@
 package ru.testikov.leet;
 
+import java.util.HashMap;
+
 /*
 The Tribonacci sequence Tn is defined as follows:
 
@@ -27,13 +29,13 @@ Constraints:
 0 <= n <= 37
 The answer is guaranteed to fit within a 32-bit integer, ie. answer <= 2^31 - 1.
  */
-public class L1137Tribonacci {
+public class L1137Tribonacci_Number {
     public static void main(String[] args) {
         System.out.println(tribonacci(7));
         System.out.println(tribonacci(25));
     }
     public static int tribonacci(int n) {
-        int a = 0, b = 1, c =  1;
+        int a = 0, b = 1, c = 1;
         if (n == 0) return a;
         if (n == 1) return b;
         if (n == 2) return c;

1b1f28a064ec245f3648c0fb351470a7ded02dcd
TyurinV <denfal486@gmail.com>
2023-04-17 23:21:32 +0300

    some fix

--- src/main/java/ru/testikov/leet/L1137Tribonacci.java
+++ src/main/java/ru/testikov/leet/L1137Tribonacci.java
@@ -33,7 +33,7 @@ public class L1137Tribonacci {
         System.out.println(tribonacci(25));
     }
     public static int tribonacci(int n) {
-        int a = 0, b = 1, c = 1;
+        int a = 0, b = 1, c =  1;
         if (n == 0) return a;
         if (n == 1) return b;
         if (n == 2) return c;

1477d6b753e91043285dae54d0e44934e34599a4
TyurinV <denfal486@gmail.com>
2023-07-02 21:08:55 +0300

    242 Valid Anagram fix

--- src/main/java/ru/testikov/leet/L242ValidAnagram.java
+++ src/main/java/ru/testikov/leet/L242ValidAnagram.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class L242ValidAnagram {
     public static void main(String[] args) {
-        System.out.println(isAnagram2("arra", "arar"));
+        System.out.println(isAnagram2("anagram", "nagaram"));
     }
 
     public static boolean isAnagram(String s, String t) {
@@ -22,13 +22,12 @@ public class L242ValidAnagram {
         if (s.length() != t.length()) return false;
         int[] counter = new int[26];
         for (int i = 0; i < s.length(); i++) {
-            counter[s.charAt(0) - 'a']++;
-            counter[t.charAt(0) - 'a']--;
+            counter[s.charAt(i) - 'a']++;
+            counter[t.charAt(i) - 'a']--;
         }
         for (int a : counter) {
             if (a != 0) return false;
         }
-
         return true;
     }
 }

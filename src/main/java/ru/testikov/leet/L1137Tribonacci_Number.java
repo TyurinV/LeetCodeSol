bbfc5844154156bf0fdd0f1d5dfcc1a32037f4e3
TyurinV <denfal486@gmail.com>
2024-04-24 22:17:00 +0300

    1137. N-th Tribonacci Number 2nd way

--- src/main/java/ru/testikov/leet/L1137Tribonacci_Number.java
+++ src/main/java/ru/testikov/leet/L1137Tribonacci_Number.java
@@ -32,8 +32,11 @@ The answer is guaranteed to fit within a 32-bit integer, ie. answer <= 2^31 - 1.
 public class L1137Tribonacci_Number {
     public static void main(String[] args) {
         System.out.println(tribonacci(7));
+        System.out.println(tribonacci2(7));
+        System.out.println(tribonacci2(25));
         System.out.println(tribonacci(25));
     }
+
     public static int tribonacci(int n) {
         int a = 0, b = 1, c = 1;
         if (n == 0) return a;
@@ -44,6 +47,19 @@ public class L1137Tribonacci_Number {
             a = b;
             b = c;
             c = sum;
+        }
+        return c;
+    }
+
+    public static int tribonacci2(int n) {
+        if (n == 0) return n;
+        if (n == 1 || n == 2) return 1;
+        int a = 0, b = 1, c = 1;
+        for (int i = 3; i <= n; i++) {
+            int next = a + b + c;
+            a = b;
+            b = c;
+            c = next;
         }
         return c;
     }

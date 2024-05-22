d1bd65447115ae13312478176bd8c14a586fcb45
TyurinV <denfal486@gmail.com>
2024-05-22 20:43:30 +0300

    202. Happy Number

---
+++ src/main/java/ru/testikov/leet/LV2/L202HappyNumber.java
@@ -0,0 +1,42 @@
+package ru.testikov.leet.LV2;
+
+import java.util.Arrays;
+
+// 6 6 6
+public class L202HappyNumber {
+    public static boolean isHappy(int n) {
+        int count = 0;
+        while (count <= 15) {
+            count++;
+            if (n == 1) return true;
+            n = (int) summm(intToArray(n));
+        }
+
+        return false;
+    }
+
+    public static int[] intToArray(int x) {
+        int[] tmp = new int[10];
+        int count = 0;
+        while (x > 0) {
+            tmp[count] = x % 10;
+            x = x / 10;
+            count++;
+        }
+        return tmp;
+    }
+
+    public static long summm(int[] num) {
+        long b = 0;
+        for (int i = 0; i < num.length; i++) {
+            b = b + num[i] * num[i];
+        }
+        return b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isHappy(1999999999));
+        System.out.println(isHappy(19));
+        System.out.println(isHappy(2));
+    }
+}

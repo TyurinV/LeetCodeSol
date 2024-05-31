e1da115d960d15e7ebea8e38b7e98699fc3151f5
TyurinV <denfal486@gmail.com>
2024-05-31 17:10:31 +0300

    389. Find the Difference

--- src/main/java/ru/testikov/leet/LV2/L389FindtheDifference.java
+++ src/main/java/ru/testikov/leet/LV2/L389FindtheDifference.java
@@ -1,15 +1,13 @@
 package ru.testikov.leet.LV2;
 
+import java.util.stream.Stream;
+
 public class L389FindtheDifference {
     public static char findTheDifference(String s, String t) {
-        int sSum = 0;
-        int tSum = 0;
-        for (char a : s.toCharArray()) {
-            sSum += a;
-        }
-        for (char a : t.toCharArray()) {
-            tSum += a;
-        }
+
+        int sSum = s.chars().sum();
+        int tSum = t.chars().sum();
+
         if (s.length() > t.length()) {
             return (char) (sSum - tSum);
         } else return (char) (tSum - sSum);

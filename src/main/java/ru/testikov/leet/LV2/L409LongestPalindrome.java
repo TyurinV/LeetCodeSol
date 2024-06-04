5984ccf5c31c5db37695f7ead7fde868e06cc93c
TyurinV <denfal486@gmail.com>
2024-06-04 20:37:43 +0300

    409. Longest Palindrome

--- src/main/java/ru/testikov/leet/LV2/L409LongestPalindrome.java
+++ src/main/java/ru/testikov/leet/LV2/L409LongestPalindrome.java
@@ -18,7 +18,7 @@ public class L409LongestPalindrome {
 
     public static void main(String[] args) {
         System.out.println(longestPalindrome("abccccdd"));
-        System.out.println(longestPalindrome("a"));
+        System.out.println(longestPalindrome("a")) ;
         System.out.println(longestPalindrome("abb"));
 
     }

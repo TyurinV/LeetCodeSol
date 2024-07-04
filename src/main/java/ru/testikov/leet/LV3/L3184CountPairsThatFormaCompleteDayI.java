c627bd9ba5dfa6bc4dba0e4be7304dd279bc2224
TyurinV <denfal486@gmail.com>
2024-07-04 22:31:03 +0300

    3184. Count Pairs That Form a Complete Day I

---
+++ src/main/java/ru/testikov/leet/LV3/L3184CountPairsThatFormaCompleteDayI.java
@@ -0,0 +1,15 @@
+package ru.testikov.leet.LV3;
+
+public class L3184CountPairsThatFormaCompleteDayI {
+    public int countCompleteDayPairs(int[] hours) {
+        int count = 0;
+        for(int i = 0; i < hours.length; i ++){
+            for(int j = i+1; j < hours.length; j ++){
+                if((hours[i] + hours[j])%24 == 0) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
